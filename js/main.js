6278828e7a3077460ffc07b38a29b53fde996d18
kidgang07 <killking113@gmail.com>
2024-02-23 18:43:09 +0700

    update tab active function for match & about section

--- js/main.js
+++ js/main.js
@@ -4,13 +4,41 @@
 
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
-
 //
 // Place any custom JS here
 //
 
 // Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new bootstrap.Popover(popover)
-  })
+document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popover) => {
+  new bootstrap.Popover(popover);
+});
+
+// tab active
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+function matchFilterActive() {
+  let tabFilter = $(".tab-filter");
+  let tabChild = tabFilter.querySelectorAll(".tab-item");
+  tabChild.forEach((tab, index) => {
+    tab.onclick = function (event) {
+      event.preventDefault();
+      tabFilter.querySelector(".tab-item.active").classList.remove("active");
+      this.classList.add("active");
+    };
+  });
+}
+
+function aboutListActive() {
+  let tabFilter = $(".toc-list");
+  let tabChild = tabFilter.querySelectorAll(".toc-item");
+  tabChild.forEach((tab, index) => {
+    tab.onclick = function () {
+      tabFilter.querySelector(".toc-item.active").classList.remove("active");
+      this.classList.add("active");
+    };
+  });
+}
+
+matchFilterActive();
+aboutListActive();
